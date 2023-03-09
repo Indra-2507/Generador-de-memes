3ef3f176c26bad8dd9634ab41f9808e7817938da
Indra Lima <indraalima@gmail.com>
2023-03-09 18:54:14 -0300

    html and css first progress

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const capitalizar=(str)=>{
+    return (str.slice(0,1).toUpperCase() + str.slice(1))
+  }
+  
+ /* <table width="80%">
+<script language="javascript">
+var r = new Array("00","33","66","99","CC","FF");
+var g = new Array("00","33","66","99","CC","FF");
+var b = new Array("00","33","66","99","CC","FF");
+for (i=0;i<r.length;i++){
+    for (j=0;j<g.length;j++) {
+        document.write("<tr>");
+        for (k=0;k<b.length;k++) {
+            var nuevoc = "#" + r[i] + g[j] + b[k];
+            document.write("<td bgcolor=\"" + nuevoc + "\" align=center>");
+            document.write(nuevoc);
+            document.write("</td>");
+        }
+        document.write("</tr>");
+    }
+}
+</script>
+</table>*/
+/*body.dark-theme
+--color-header: #191e2b;
+--color-background: #30343f;
+--color-panel: #252935;
+--color-boton-default: #393e49;
+--color-boton-default-hover: #4e5565;
+--color-text: #ffffff;
+--font-primary: 'Open Sans', sans-serif;
+--border-radius: 3px;*/
