718e33e3f5a62a1df692edfae069753eed67d6ad
Indra Lima <indraalima@gmail.com>
2023-03-13 16:26:07 -0300

    adding javascript functions for aside-text

--- js/main.js
+++ js/main.js
@@ -27,15 +27,110 @@ $("#url").addEventListener("input", () => {
     const imageBack= $("#color").value
     $(".main__div--center").style.backgroundColor=imageBack
   } )
+
+
+
+
+
   //no sale
+$("#lighten").addEventListener("click",()=>{
+    const optionValue= $("#lighten").value
+  $(".main__div--center").style.mixBlendMode=`lighten(${optionValue})`
+})
+
 $("#darken").addEventListener("input",()=>{
-  if("#darken".checked){
-  $(".main__div--center").style.backgroundMixBlendMode=$("#darken").value}
+  const optionValue= $("#darken").value
+$(".main__div--center").style.backgroundBlendMode=`darken(${optionValue})`
 })
 
-$("#brigthness__slider").addEventListener("input",()=>{
-  const filterValue= $("#brigthness__slider").value
-  $(".main__div--center").style.filter= 
-filterValue
+
+
+
+
+
+
+
+
+//filter slide
+$("#brigthness__slider").addEventListener("input", () => {
+       const rangeValue = $("#brigthness__slider").value
+    $(".main__div--center").style.filter = `brightness(${rangeValue}%)`
+   })
+   $("#opacity__slider").addEventListener("input", () => {
+    const rangeValue = $("#opacity__slider").value
+ $(".main__div--center").style.filter = `opacity(${rangeValue}%)`
+})
+$("#contrast__slider").addEventListener("input", () => {
+  const rangeValue = $("#contrast__slider").value
+$(".main__div--center").style.filter = `contrast(${rangeValue}%)`
+})
+$("#blur__slider").addEventListener("input", () => {
+  const rangeValue = $("#blur__slider").value
+$(".main__div--center").style.filter = `blur(${rangeValue}%)`
+})
+$("#grayscale__slider").addEventListener("input", () => {
+  const rangeValue = $("#grayscale__slider").value
+$(".main__div--center").style.filter = `grayscale(${rangeValue}%)`
+})
+$("#sepia__slider").addEventListener("input", () => {
+  const rangeValue = $("#sepia__slider").value
+$(".main__div--center").style.filter = `sepia(${rangeValue}%)`
+})
+$("#hue__slider").addEventListener("input", () => {
+  const rangeValue = $("#hue__slider").value
+$(".main__div--center").style.filter = `hue(${rangeValue}%)`
+})
+$("#saturate__slider").addEventListener("input", () => {
+  const rangeValue = $("#saturate__slider").value
+$(".main__div--center").style.filter = `saturate(${rangeValue}%)`
+})
+$("#invert__slider").addEventListener("input", () => {
+  const rangeValue = $("#invert__slider").value
+$(".main__div--center").style.filter = `invert(${rangeValue}%)`
 })
 
+
+//type reset??
+
+//text form
+
+$("#topText").addEventListener("input", ()=>{
+  $(".main__top").innerText = $("#topText").value
+})
+
+$("#outTextTop").addEventListener("click", () => {
+  if ($("#outTextTop").checked ){
+    $(".main__blackTop").style.display="none"
+    $(".main__div--center").style.height="550px"
+  }
+    else{
+      $(".main__blackTop").style.display="block"
+ }
+})
+
+$("#bottomText").addEventListener("input", ()=>{
+  $(".main__bottom").innerText = $("#bottomText").value
+})
+
+$("#outTextBottom").addEventListener("click", () => {
+  if ($("#outTextBottom").checked ){
+    $(".main__blackBottom").style.display="none"
+    $(".main__div--center").style.height="550px"
+  }
+    else{
+      $(".main__blackBottom").style.display="block"
+    }
+})
+
+//fuentes
+
+
+ $("#fonts").addEventListener("input", () => {
+  $(".main__top").style.fontFamily = $("#fonts").value
+     $(".main__bottom").style.fontFamily = $("#fonts").value
+ })
+
+//america, andale mono, comics sanz
+$("#fontValue").addEventListener("click",() =>{
+
+})
